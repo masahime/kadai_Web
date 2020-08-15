5db7c5806ea5c63a5efe7a7c232ea4989d2005e3
masahime <66562122+masahime@users.noreply.github.com>
2020-08-15 18:44:42 +0900

    Add files via upload

---
+++ web_tech/goya.rb
@@ -0,0 +1,25 @@
+require 'cgi'
+cgi = CGI.new
+# データを受け取った後、HTMLの形式でレスポンスを返す
+if cgi.key?('goya')
+  cgi.out("type" => "text/html", "charset" => "UTF-8") {
+  # 情報の目印とした"goya"のデータを cgi['goya']と言う記述で取り出し、ローカル変数に代入する
+    get = cgi['goya']
+    "<html>
+      <body>
+        <p>ゴーヤの大きさと売った相手の情報は下記になります</p>
+        文字列：#{get}
+      </body>
+    </html>"
+  }
+elsif cgi.key?('goya1')
+  cgi.out("type" => "text/html", "charset" => "UTF-8") {
+    get_1 = cgi['goya1']
+    "<html>
+      <body>
+        <p>譲渡先が自家消費でないゴーヤの情報は下記になります</p>
+        文字列：#{get_1}
+      </body>
+    </html>"
+  }
+end
